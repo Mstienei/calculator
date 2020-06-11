08bbfa2d3c3a50b3bff1486e78b40066367e8028
Maureen <mstienei@gmail.com>
2020-06-11 12:12:10 +0300

    update footer and output text

--- js/scripts.js
+++ js/scripts.js
@@ -21,27 +21,27 @@ event.preventDefault();
 var number1 = parseInt($("#add1").val());
 var number2 = parseInt($("#add2").val());
 var result = add(number1, number2);
-$("#output").text(result);
+$("#output").text("Answer =" + result);
 });
 $("form#subtract").submit(function(event) {
 event.preventDefault();
 var number1 = parseInt($("#subtract1").val());
 var number2 = parseInt($("#subtract2").val());
 var result = subtract(number1, number2);
-$("#output").text(result);
+$("#output").text("Answer =" + result);
 });
 $("form#multiply").submit(function(event) {
 event.preventDefault();
 var number1 = parseInt($("#multiply1").val());
 var number2 = parseInt($("#multiply2").val());
 var result = multiply(number1, number2);
-$("#output").text(result);
+$("#output").text("Answer =" + result);
 });
 $("form#divide").submit(function(event) {
 event.preventDefault();
 var number1 = parseInt($("#Divide1").val());
 var number2 = parseInt($("#Divide2").val());
 var result = divide(number1, number2);
-$("#output").text(result);
+$("#output").text("Answer =" + result);
 });
 });
